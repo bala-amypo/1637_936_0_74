429fb3b10a6c43186e10217da170dd89cc81b0a8
bala-amypo <bala@amypo.in>
2025-12-18 08:58:29 +0000

    User commit

--- src/main/java/com/example/demo/repository/ValidationRepository.java
+++ src/main/java/com/example/demo/repository/ValidationRepository.java
@@ -1,3 +1,3 @@
 package com.example.demo.respository;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repo
+import org.springframework.data.jpa.repository.JpaRepository
