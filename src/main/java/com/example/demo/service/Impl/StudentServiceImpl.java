8332dc398b5beccb481ebc1df20f646e6e46bba0
bala-amypo <bala@amypo.in>
2025-12-18 06:10:20 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/StudentServiceImpl.java
@@ -34,4 +34,9 @@ public class StudentServiceImpl implements StudentService{
     public StudentEntity getData(int id ){
         return student.findById(id).orElse(null);                                                                                  
     }
+    @Override
+    public StudentEntity updateData(int id,StudentEntity entity){
+        
+
+    }
 }
